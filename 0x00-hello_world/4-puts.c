1aed609a1c15b6b6d1d0ea12001353550be29816
Emmy042 <Emmybankzz7@gmail.com>
2024-06-13 20:16:49 +0100

    first C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+*main - print without using printf
+*Return: returns 0 meaning their is no error
+*/
+int main(void)
 {
 puts("programming is like building multilingual puzzle");
-return 0;
+
+return (0);
 }
